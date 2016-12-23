a8e6b935a01f6fdc5343a11a8b2614270b3da5d2
wcc0305 <220150824@seu.edu.cn>
2016-12-23 11:20:21 +0800

    version1.0

--- config.py
+++ config.py
@@ -41,8 +41,30 @@ class ProductionConfig(Config):
                               'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
+
+class HerokuConfig(ProductionConfig):
+    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # handle proxy server headers
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-    'default': DevelopmentConfig}
+    'heroku': HerokuConfig,
+    'default': DevelopmentConfig
+}
