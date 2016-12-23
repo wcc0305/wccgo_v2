c66d4c4e6de235ccd6d4dac2bf41e8d0c5605333
wcc0305 <220150824@seu.edu.cn>
2016-12-23 13:01:00 +0800

    version1.0

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'wcc <13675157322@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
@@ -19,8 +22,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 25
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
