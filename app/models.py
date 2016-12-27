15454d24ee650c54413ea676f86ccdace554505a
wcc0305 <220150824@seu.edu.cn>
2016-12-27 17:59:52 +0800

    version1.0

--- app/models.py
+++ app/models.py
@@ -186,10 +186,15 @@ class Post(db.Model):
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
+                        'h1', 'h2', 'h3', 'p', 'img']
+        attrs = {
+            '*': ['class', 'style'],
+            'a': ['href', 'rel'],
+            'img': ['alt', 'src'],
+        }
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
-            tags=allowed_tags, strip=True))
+            tags=allowed_tags, attributes=attrs, strip=True))
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 
