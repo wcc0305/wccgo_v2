d0d2d5e00c8ed08a79dfce98844269f79f4fd38f
wcc0305 <220150824@seu.edu.cn>
2017-01-03 16:34:04 +0800

    v2最终

--- app/main/forms.py
+++ app/main/forms.py
@@ -68,7 +68,7 @@ class TagListField(Field):
             for obj in self.data:
                 r += self.obj_to_str(obj)
                 r += ';'    #以;为间隔
-            return r    #参考代码里有问题，已修改
+            return r[:-1]    #参考代码里有问题，已修改
         else:
             return ''
 
